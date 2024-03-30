005492d2e0c590ec719ffe511f3b128fbbd0bcfd
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-03-30 18:33:41 +0000

    Solve 03 with reverse string method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let split_str = str.split("");
+    split_str.reverse();
+    return split_str.join('');
 };
 
 // Do not edit below this line
