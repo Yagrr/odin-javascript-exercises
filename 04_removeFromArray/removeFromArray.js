44e32ccf010faabede80a9b555694ee9e62013e8
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-03-30 23:48:30 +0000

    Solve 04, full pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(input_array, ...elements) {
+    /* implemented with two stacks where argument[0] is the array
+     and following elements are to be removed from array */
+    let array = input_array;
+    for (const element of elements) {
+        let index = array.indexOf(element);
+        // use while loop to remove all instance of element
+        while (index > -1) {
+            array.splice(index, 1)
+            index = array.indexOf(element);
+        }
+    }
+    return array
+}
 
-};
-
-// Do not edit below this line
+// Do not edit below this li<F3>ne
 module.exports = removeFromArray;
