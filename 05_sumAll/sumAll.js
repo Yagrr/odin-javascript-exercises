3c8843954495801e689353e8c63367d3733bb25f
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-03-31 13:32:03 +0100

    Solve 05 with sum stack implementation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(int1,int2) {
+    if (Number.isInteger(int1) && Number.isInteger(int2)  && int1 >= 0 && int2 >= 0) {
+        if (int2 < int1) {
+            // swap ints
+            int1 = [int2, int2 = int1][0];
+        }
+        let arr= [];
+        while (int1 <= int2) {
+            arr.push(int1++);
+        }
+        let sum = arr.reduce((x,y) => x + y, 0);
+        return sum;
+    }
+    else {return 'ERROR'}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
