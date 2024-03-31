c883a91d6e0fe95c11f7829b5488734bc2547a5d
Yagrr <90156764+Yagrr@users.noreply.github.com>
2024-03-31 14:23:11 +0100

    Solve 06 with AND OR logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,9 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(year) {
+    //  x % 4 = 0 && !x % 100 = 0 && x % 400 = 0
+    if ( ((year % 4 ) == 0 && (year % 100) != 0) || (year % 400) == 0) {
+        return true
+    }
+    else return false;
+}
 // Do not edit below this line
 module.exports = leapYears;
